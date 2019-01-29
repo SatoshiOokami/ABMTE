2aae6d1fad59bcc69baada71ce9b886b2f0aff13
Satoshi Ookami <satoshigray.sama@gmail.com>
2019-01-29 16:20:56 +0100

    Changing service name

---
+++ src/app/anime.ts
@@ -0,0 +1,16 @@
+export class Anime {
+    mal_id: number;
+    url: string;
+    image_url: string;
+    title: string;
+    title_english: string;
+    title_japanese: string;
+    type: string;
+    source: string;
+    status: string;
+    episodes: number;
+    score: number;
+    synopsis: string;
+    airedFrom: Date;
+    airedTo: Date;
+  }
