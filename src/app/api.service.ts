5d1241a08a8476c2fecd9d4467fb1496d5b0477d
Satoshi Ookami <satoshigray.sama@gmail.com>
2019-01-30 10:41:00 +0100

    Home Page styling

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Anime } from './anime';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = "https://api.jikan.moe/v3";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
+  public getAnime(id): Observable<Anime> {
+    const url = `${apiUrl}/anime/${id}`;
+    return this.http.get<Anime>(url).pipe(
+      tap(_ => console.log(`fetched product id=${id}`)),
+      catchError(this.handleError<Anime>(`getProduct id=${id}`))
+    );
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
-import { ApiService } from '../api-service.service';
+import { ApiService } from '../api.service';
+import { Anime } from '../anime';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  providers: [ApiService]
 })
+
 export class Tab2Page {
+  public anime: Anime;
+
+  constructor(public ApiService: ApiService){}
+
   /**
    * testFunc
    */
-  public testFunc() {
-    console.log("Hello");
+  public testFunc(id) {
+    this.ApiService.getAnime(id).subscribe(
+      (response) => {
+        this.anime = response;
+        console.log(this.anime);
+      }
+    );
   }
 }
