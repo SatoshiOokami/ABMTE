c0adecb3c0d1c89a1031bc93821b3cdb48e70ff2
Satoshi Ookami <satoshigray.sama@gmail.com>
2019-02-03 22:19:34 +0100

    Favorite tab finished

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,21 +1,46 @@
 import { Component } from '@angular/core';
 import { Storage, IonicStorageModule } from '@ionic/storage';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
-  providers: [IonicStorageModule]
+  providers: [IonicStorageModule, NavController]
 })
 export class Tab4Page {
 
-  constructor(private storage: Storage) { }
-  private favoriteFields: Array<any>;
+  constructor(private storage: Storage, private navCtrl: NavController) {}
+  private favoriteItems: Array<any>;
+  private anime: Array<any>;
+  private manga: Array<any>;
+  private characters: Array<any>;
+  private persons: Array<any>;
 
   ionViewDidEnter() {
     this.storage.get("favoriteItems").then((data)=> {
-      this.favoriteFields = data;
+      this.favoriteItems = data;
       console.log(data);
+
+      this.anime = this.favoriteItems.filter((item) => {
+        return item.category == "anime";
+      });
+
+      this.manga = this.favoriteItems.filter((item) => {
+        return item.category == "manga";
+      });
+      
+      this.characters = this.favoriteItems.filter((item) => {
+        return item.category == "character";
+      });
+      
+      this.persons = this.favoriteItems.filter((item) => {
+        return item.category == "person";
+      });
     });
+  }
+  
+  public passSelectedValue(category, id) {
+    this.navCtrl.navigateForward('tabs/tab3?type=' + category + '&id=' + id);
   }
 }
